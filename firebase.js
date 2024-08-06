bd6bdc7ca028b9f9c33f7be206c48c36230a81a0
SMei201900 <smei201900@gmail.com>
2024-08-06 01:00:22 -0400

    removed firebase api's bc lacking security code at the moment

--- firebase.js
+++ firebase.js
@@ -8,15 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: "AIzaSyCABbKrd1YtkkW_6Es6mTgw6S8nkKzJ8C0",
-	authDomain: "inventory-management-29355.firebaseapp.com",
-	projectId: "inventory-management-29355",
-	storageBucket: "inventory-management-29355.appspot.com",
-	messagingSenderId: "717078527862",
-	appId: "1:717078527862:web:0832783c17742fd2634457",
-	measurementId: "G-R7LMM5HFKK",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
